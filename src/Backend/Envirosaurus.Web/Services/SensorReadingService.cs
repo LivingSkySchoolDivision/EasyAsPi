089085c05cef335c2a22e9e4be5df7bb82a6c7b3
Mark Strendin <mark.strendin@lskysd.ca>
2022-04-04 10:22:06 -0600

    Add a few extra API endpoints, record last seen version from heartbeat (#1)
    * Start working on a web management system
    
    * Add API endpoints for getting sensors via different means
    
    * Record last seen version number from heartbeats
    And store version number more centrally

---
+++ src/Backend/Envirosaurus.Web/Services/SensorReadingService.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Envirosaurus.Web.Services;
+
+public class SensorReadingService
+{
+    private readonly IRepository<SensorReading> _repository;
+    public SensorReadingService (IRepository<SensorReading> Repository) 
+    {
+        this._repository = Repository;
+    }
+
+    public IEnumerable<SensorReading> GetAll()
+    {
+        return this._repository.GetAll();
+    }
+
+    public SensorReading Get(Guid guid)
+    {
+        return this._repository.GetById(guid);
+    }
+
+    public IEnumerable<SensorReading> GetForSensor(Sensor sensor)
+    {
+        return this._repository.Find(x => x.DeviceSerialNumber == sensor.DeviceSerialNumber);
+    }
+
+    public void Insert(SensorReading Reading)
+    {
+        this._repository.Insert(Reading);
+    }
+
+}
