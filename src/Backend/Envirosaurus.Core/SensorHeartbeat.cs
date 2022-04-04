04bf25551c6fc8c6f8d3146f478437e74257daed
Mark Strendin <mark.strendin@lskysd.ca>
2022-04-04 14:34:36 -0600

    Don't require the device version, so older clients don't crash

--- src/Backend/Envirosaurus.Core/SensorHeartbeat.cs
+++ src/Backend/Envirosaurus.Core/SensorHeartbeat.cs
@@ -5,6 +5,5 @@ public class SensorHeartbeat
     public string DeviceSerialNumber { get; set; }
     public string DeviceModel { get; set; }
     public DateTime TimestampUTC { get; set; }
-    public string DeviceVersion { get; set; }
-
+    public string? DeviceVersion { get; set; }
 }
