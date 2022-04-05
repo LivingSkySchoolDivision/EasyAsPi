5b75c503aa43e998602cebb59a6bcd0afe1cec43
Mark Strendin <mark.strendin@lskysd.ca>
2022-04-05 16:36:06 -0600

    Create a basic web interface

--- src/Backend/Envirosaurus.Core/GlobalSettings.cs
+++ src/Backend/Envirosaurus.Core/GlobalSettings.cs
@@ -3,4 +3,6 @@ namespace Envirosaurus;
 public static class GlobalSettings
 {
     public const string Version = "EASYASPI004";
+    public const string ProductName = "Easy as Pi";
+    public const string ProductSubName = "A Raspberry Pi based environment sensor system";
 }
