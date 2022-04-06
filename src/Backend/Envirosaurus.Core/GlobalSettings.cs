37307f04a763eafe5bdf5619e738264c90ed04c3
Mark Strendin <mark.strendin@lskysd.ca>
2022-04-06 11:42:18 -0600

    Support time zones instead of UTC (#3)

--- src/Backend/Envirosaurus.Core/GlobalSettings.cs
+++ src/Backend/Envirosaurus.Core/GlobalSettings.cs
@@ -5,4 +5,5 @@ public static class GlobalSettings
     public const string Version = "EASYASPI004";
     public const string ProductName = "Easy as Pi";
     public const string ProductSubName = "A Raspberry Pi based environment sensor system";
+    public static TimeZoneInfo TimeZone = TimeZoneInfo.FindSystemTimeZoneById("Etc/UTC");
 }
